8bf42b83fd7e56854af70363417e4ce49150e149
AlexRubio14 <alejandro.rubio@enti.cat>
2023-03-02 00:31:00 +0100

    Trying movement

--- Assets/Scripts/PlayerScripts/playerController.cs
+++ Assets/Scripts/PlayerScripts/playerController.cs
@@ -12,34 +12,50 @@ public class playerController : MonoBehaviour
 
     private Rigidbody2D rb2d;
 
-    private float movementDirection;
 
+    private float movementDirection;
+    bool isJumping = false;
 
     private void Awake()
     {
       rb2d = GetComponent<Rigidbody2D>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        transform.position = new Vector2(speed * Time.deltaTime, 0.0f);
+      
     }
 
 
     private void FixedUpdate()
     {
-        if(Input.GetKey(KeyCode.D))
-            rb2d.AddForce(transform.right, ForceMode2D.Impulse);
-        else if (Input.GetKey(KeyCode.A))
-            rb2d.AddForce(transform.right * -1, ForceMode2D.Impulse);
-        if(Input.GetKey(KeyCode.Space))
-            rb2d.AddForce(transform.up, ForceMode2D.Impulse);
+        isJumping = false;
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        if (horizontalMovement == 1)
+        {
+            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+        }
+        else if (horizontalMovement == -1)
+        {
+            rb2d.velocity = new Vector2(-speed, rb2d.velocity.y);
+        }
+        else
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        
+        
+        
+        if(Input.GetKeyDown("space") && !isJumping)
+        {
+            //Apply JumpForce
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+            isJumping = true;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision)
+        if(collision.gameObject.CompareTag("Floor"))
+        {
+            isJumping = false;
+        }
+        //We cancell the rest of force of jump
+        rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
     }
 
 }
