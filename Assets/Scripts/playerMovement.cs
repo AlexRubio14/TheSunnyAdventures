49e42a10cd1139fa94883e25fe0a3950196558f2
Pau Giro <pau.giro@enti.cat>
2023-03-01 13:52:46 +0100

    Tiles & Movement

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,19 +4,56 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    private Rigidbody2D rg2d;
     [SerializeField]
-    private int velocity;  
+    private float speed;
+    [SerializeField]
+    private float jumpForce;
 
-    // Start is called before the first frame update
+    [SerializeField]
+    private LayerMask floorLayer;
+
+    private float horizontal;
+    private Rigidbody2D rb2d;
+    private bool Grounded;
+
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
     void Start()
     {
-        rg2d = GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if (Physics2D.Raycast(transform.position, Vector2.down, 0.6f, floorLayer))
+        {
+            Grounded = true;
+
+        }
+        else Grounded = false;
+
+        if (Input.GetKeyDown(KeyCode.Space) && Grounded)
+        {
+            Jump();
+        }
     }
+
+    private void Jump()
+    {
+        rb2d.AddForce(Vector2.up * jumpForce);
+    }
+
+
+    private void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(horizontal * speed, rb2d.velocity.y);
+    }
+
+
 }
