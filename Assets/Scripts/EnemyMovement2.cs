37a830b2c3c438abeb8f929ff42680ee436f203d
Marcos <marcos.ruiz@enti.cat>
2023-03-03 17:00:02 +0100

    Enemy 2 movement

--- Assets/Scripts/EnemyMovement2.cs
+++ Assets/Scripts/EnemyMovement2.cs
@@ -14,7 +14,6 @@ public class EnemyMovement2 : MonoBehaviour
     private float minX;
 
     public bool fliped = false;
-    private SpriteRenderer sp;
 
 
     Rigidbody2D myRigidBody;
@@ -22,41 +21,36 @@ public class EnemyMovement2 : MonoBehaviour
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
-        sp = GetComponent<SpriteRenderer>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (IsFacingRight())
+        if (transform.position.x < maxX && fliped == false)
         {
             myRigidBody.velocity = new Vector2(enemyMovement, 0f);
-         
-            if(transform.position.x == maxX)
+            if(transform.position.x > maxX-0.1)
             {
-                Debug.Log("oalsjhdsihf");
-                sp.flipX = false;
+                fliped = true;
+            }
+           
+            
+
+
+        }
+        else if (transform.position.x > minX && fliped == true)
+        {
+           
+            myRigidBody.velocity = new Vector2(-enemyMovement, 0f);
+            if (transform.position.x < minX + 0.1)
+            {
+                fliped = false;
             }
 
         }
-        else if (!IsFacingRight())
-        {
-            myRigidBody.velocity = new Vector2(-enemyMovement, 0f);
-            
-            if (transform.position.x == minX)
-            {
-                sp.flipX = true;
-            }
-        }
     }
-    private bool IsFacingRight()
-    {
-        return transform.localScale.x > Mathf.Epsilon;
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), transform.localScale.y);
-    }
+   
 
    
     
