5c9832ca6bcf040f82dd34afa610eaa1d6b6550e
Marcos <marcos.ruiz@enti.cat>
2023-03-05 16:24:33 +0100

    movement

--- Assets/Scripts/EnemyMovement2.cs
+++ Assets/Scripts/EnemyMovement2.cs
@@ -13,39 +13,34 @@ public class EnemyMovement2 : MonoBehaviour
     [SerializeField]
     private float minX;
 
-    public bool fliped = false;
-
-
+    SpriteRenderer sr;
     Rigidbody2D myRigidBody;
 
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
-      
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < maxX && fliped == false)
+        if (transform.position.x < maxX && sr.flipX == false)
         {
             myRigidBody.velocity = new Vector2(enemyMovement, 0f);
             if(transform.position.x > maxX-0.1)
             {
-                fliped = true;
+                sr.flipX = true;
             }
            
-            
-
-
         }
-        else if (transform.position.x > minX && fliped == true)
+        else if (transform.position.x > minX && sr.flipX == true)
         {
            
             myRigidBody.velocity = new Vector2(-enemyMovement, 0f);
             if (transform.position.x < minX + 0.1)
             {
-                fliped = false;
+                sr.flipX = false;
             }
 
         }
