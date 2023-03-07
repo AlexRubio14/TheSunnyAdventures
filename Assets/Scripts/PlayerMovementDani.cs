299791f91b9cd5ba89892339a23e3dc0beef7c10
Dani <daniel.bailon@enti.cat>
2023-03-07 10:22:25 +0100

    Dash & Player movement

---
+++ Assets/Scripts/PlayerMovementDani.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementDani : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private PlayerDash playerDash;
+
+    [Header("Movement")]
+    [SerializeField]
+    private float speed = 4f;
+    private float direction;
+    public float Direction => direction;
+
+    [Header("Jump")]
+    [SerializeField]
+    private float jumpForce = 4f;
+    [SerializeField]
+    private Transform checkGround;
+    [SerializeField]
+    private float raycastLength;
+    [SerializeField]
+    private LayerMask groundLayer;
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerDash = GetComponent<PlayerDash>();
+    }
+    
+    void Update()
+    {
+        direction = Input.GetAxisRaw("Horizontal");
+        if (!playerDash.IsDashing)
+        {
+            Jump();
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (!playerDash.IsDashing)
+        {
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
+    }
+    
+    private void Jump()
+    {
+        isGrounded = Physics2D.Raycast(checkGround.position, Vector2.down, raycastLength, groundLayer);
+
+        if(Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+ 
+}
