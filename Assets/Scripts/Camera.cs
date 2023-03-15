5097ddd46b698fa5d4edd6bb8db16d054a40b628
Marcos <marcos.ruiz@enti.cat>
2023-03-15 21:30:24 +0100

    Door

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     public GameObject virtualCam;
+    public GameObject door;
+    public GameObject virtualCam4;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player") && !other.isTrigger)
         {
             virtualCam.SetActive(true);
+        }
+        if(virtualCam4 == true)
+        {
+            door.SetActive(true);
         }
     }
 
@@ -21,4 +27,6 @@ public class Camera : MonoBehaviour
             virtualCam.SetActive(false);
         }
     }
+
+    
 }
