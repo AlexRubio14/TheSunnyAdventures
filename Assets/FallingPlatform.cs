2218206987c004ef98c9db80d702efc0ee9e1a47
Marcos <marcos.ruiz@enti.cat>
2023-03-22 15:31:14 +0100

    FallingPlatform 0.75

--- Assets/FallingPlatform.cs
+++ Assets/FallingPlatform.cs
@@ -4,43 +4,58 @@ using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+
     [SerializeField]
     private float fallDelay = 3f;
     [SerializeField]
-    private float destroyDelay = 2f;
+    private float destroyDelay = 3f;
     float timeWasted;
     float timeWaited;
     bool colisionActivada = false;
+    bool empiezaContador = false;
 
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();  
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             colisionActivada = true;
-        }
-       
-        
+        }      
     }
 
     private void Update()
     {
-        if(colisionActivada== true)
+        if (colisionActivada == true && empiezaContador == false)
         {
             timeWasted += Time.deltaTime;
+   
             if (timeWasted >= fallDelay)
             {
+              
                 timeWasted = 0;
-                colisionActivada = false;   
-                gameObject.SetActive(false);
+                colisionActivada = false;
+                empiezaContador = true;
+                spriteRenderer.enabled = false;
+                boxCollider2D.enabled = false;
             }
         }
-        if(colisionActivada== false && gameObject==false)
+        if(colisionActivada == false && empiezaContador == true)
         {
             timeWaited += Time.deltaTime;
-            if(timeWaited >= destroyDelay)
+     
+            if (timeWaited >= destroyDelay)
             {
+           
                 timeWaited = 0;
-                gameObject.SetActive(true);
+                empiezaContador = false;
+                spriteRenderer.enabled = true;
+                boxCollider2D.enabled = true;
             }
         }
     }
