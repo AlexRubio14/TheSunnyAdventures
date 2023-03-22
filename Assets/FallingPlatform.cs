864b4365df5a7f36a3ba910dec3b3fcf37e12661
Marcos <marcos.ruiz@enti.cat>
2023-03-22 09:02:43 +0100

    FallingPlatform 0.5

--- Assets/FallingPlatform.cs
+++ Assets/FallingPlatform.cs
@@ -9,6 +9,7 @@ public class FallingPlatform : MonoBehaviour
     [SerializeField]
     private float destroyDelay = 2f;
     float timeWasted;
+    float timeWaited;
     bool colisionActivada = false;
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -25,12 +26,22 @@ public class FallingPlatform : MonoBehaviour
     {
         if(colisionActivada== true)
         {
-                timeWasted += Time.deltaTime;
-                if (timeWasted >= fallDelay)
-                {
-                    timeWasted = 0;
-                   gameObject.SetActive(false);
-               }
+            timeWasted += Time.deltaTime;
+            if (timeWasted >= fallDelay)
+            {
+                timeWasted = 0;
+                colisionActivada = false;   
+                gameObject.SetActive(false);
+            }
+        }
+        if(colisionActivada== false && gameObject==false)
+        {
+            timeWaited += Time.deltaTime;
+            if(timeWaited >= destroyDelay)
+            {
+                timeWaited = 0;
+                gameObject.SetActive(true);
+            }
         }
     }
 }
