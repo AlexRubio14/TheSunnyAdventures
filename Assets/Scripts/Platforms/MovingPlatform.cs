4f9bf5f84f8553b4a9376d2a56b6534ce69fd34a
Marcos <marcos.ruiz@enti.cat>
2023-03-31 10:58:35 +0200

    Moving Platforms

---
+++ Assets/Scripts/Platforms/MovingPlatform.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    playerController playerController;
+    Rigidbody2D rb2d;
+
+    [SerializeField]
+    private float maxX;
+    [SerializeField]
+    private float speed;
+
+    private bool active;
+    [SerializeField]
+    private float timer;
+    [SerializeField]
+    private float delayDestroy;
+
+
+    private void Awake()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<playerController>();
+        rb2d = GetComponent<Rigidbody2D>();
+        active = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("AttackPoint")) //&& playerController.GetAnimAttack())
+        {
+            active = true;
+        }
+    }
+
+    private void Update()
+    {
+        if(active == true)
+        {
+            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+            if(transform.position.x >= maxX)
+            {
+                speed = 0;
+                timer += Time.deltaTime;
+                if(timer >= delayDestroy)
+                {
+                    timer = 0;
+                    Destroy(gameObject);
+                }
+                
+            }
+        }
+    }
+}
