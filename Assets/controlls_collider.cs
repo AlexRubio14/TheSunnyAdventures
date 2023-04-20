fd24f8317f9f18a88d122c11536ca14c7e5f23ef
AlexRubioPlana14 <alejandro.rubio@enti.cat>
2023-04-20 13:32:04 +0200

    Canvas'finished

--- Assets/controlls_collider.cs
+++ Assets/controlls_collider.cs
@@ -6,8 +6,12 @@ public class controlls_collider : MonoBehaviour
 {
     [SerializeField]
     GameObject text_controller;
+    [SerializeField]
+    Animator _animator;
+
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        text_controller.SetActive(false);
+        _animator.SetBool("PassTrigger", true);
     }
 }
