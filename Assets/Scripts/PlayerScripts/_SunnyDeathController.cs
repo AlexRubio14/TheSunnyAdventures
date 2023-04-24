30276390c11c3ab977ded0040dd23ce74b493931
Marcos <marcos.ruiz@enti.cat>
2023-04-24 13:13:41 +0200

    Working on still not finished

--- Assets/Scripts/PlayerScripts/_SunnyDeathController.cs
+++ Assets/Scripts/PlayerScripts/_SunnyDeathController.cs
@@ -4,21 +4,17 @@ using UnityEngine;
 
 public class _SunnyDeathController : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+   private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Enemy"))
-            //Destroy(gameObject);
-        if (collision.collider.CompareTag("Spikes"))
-            Destroy(gameObject);
-       
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.CompareTag("Water"))
         {
-            Debug.Log("Si");
+            //Destroy(gameObject);
+        }
+
+        if (collision.collider.CompareTag("Spikes"))
+        {
             Destroy(gameObject);
-            
         }
     }
+
 }
--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class Water : MonoBehaviour
 {
-    
+    private BoxCollider2D bx2D;
+    private CapsuleCollider2D cc2D;
+
+    private void Start()
+    {
+        bx2D = GetComponent<BoxCollider2D>();
+        cc2D = GetComponent<CapsuleCollider2D>();
+    }
+
 }
