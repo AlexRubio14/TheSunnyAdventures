be217014118708d28d8833705e1ab1d4c61b20e6
AlexRubio14 <alejandro.rubio@enti.cat>
2023-04-30 14:27:26 +0200

    hub, star anim, title screen

--- Assets/Scripts/TitlesScreen.cs
+++ Assets/Scripts/TitlesScreen.cs
@@ -10,6 +10,10 @@ public class TitlesScreen : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void Options()
+    {
+
+    }
     public void ExitGame()
     {
         Application.Quit();
